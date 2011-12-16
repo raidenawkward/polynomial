c26a7949ee3e7510662823adae1f1222ebd4ed10
raiden <huangtao@huangtao-jinke.(none)>
2011-12-16 18:08:33 +0800

    Polynomial defined

--- polynomial.h
+++ polynomial.h
@@ -1,6 +1,23 @@
 #ifndef _POLYNOMIAL_H
 #define _POLYNOMIAL_H
 
+/*
+#define MAX_DEGREE 101
 
+struct Polynomial {
+	int degree;
+	float coef[MAX_DEGREE];
+;
+
+Polynomial Zero();
+Boolean IsZero();
+Coefficient Coef(poly,expon);
+Exponent Lead_Exp(poly)		// returns max exponent of poly
+Polynomial Attach(poly, coef, expon);
+Polynomial Remove(poly, expon);
+Polynomial SignleMult(poly, coef, expon);	// poly*coef*x^expon
+Polynomial Add(poly1, poly2);
+Polynomial Mult(poly1, poly2);	// A(x)*B(x) = SIGMA(ai*x^i * SIGMA(bj*x^j))
+*/
 
 #endif // _POLYNOMIAL_H
